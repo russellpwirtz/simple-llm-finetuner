9a43a79476bf97b1d5237799b6b4bebd87b6e008
Russ Wirtz <russ@mail.com>
2023-04-13 16:50:55 -0700

    testing models

--- config.py
+++ config.py
@@ -6,30 +6,45 @@ DEVICE = torch.device('cuda' if HAS_CUDA else 'cpu')
 
 parser = argparse.ArgumentParser(description='Simple LLM Finetuner')
 parser.add_argument('--models', nargs='+', default=[
-    'decapoda-research/llama-7b-hf', 
-    'cerebras/Cerebras-GPT-2.7B', 
-    'cerebras/Cerebras-GPT-1.3B', 
-    'EleutherAI/gpt-neo-2.7B'
+    'decapoda-research/llama-7b-hf',
+    'cerebras/Cerebras-GPT-2.7B',
+    'cerebras/Cerebras-GPT-1.3B',
+    'EleutherAI/gpt-neo-2.7B',
+    'anon8231489123/vicuna-13b-GPTQ-4bit-128g',
+    'eachadea/ggml-vicuna-13b-1.1',
+    'eachadea/legacy-ggml-vicuna-7b-4bit',
+    'tloen/alpaca-lora-7b'
 ],  help='List of models to use')
 
-parser.add_argument('--device-map', type=str, default='', help='Device map to use')
-parser.add_argument('--model', type=str, default='cerebras/Cerebras-GPT-2.7B', help='Model to use')
-parser.add_argument('--max-seq-length', type=int, default=256, help='Max sequence length')
-parser.add_argument('--micro-batch-size', type=int, default=12, help='Micro batch size')
-parser.add_argument('--gradient-accumulation-steps', type=int, default=8, help='Gradient accumulation steps')
+parser.add_argument('--device-map', type=str,
+                    default='', help='Device map to use')
+parser.add_argument('--model', type=str,
+                    default='cerebras/Cerebras-GPT-2.7B', help='Model to use')
+parser.add_argument('--max-seq-length', type=int,
+                    default=256, help='Max sequence length')
+parser.add_argument('--micro-batch-size', type=int,
+                    default=12, help='Micro batch size')
+parser.add_argument('--gradient-accumulation-steps', type=int,
+                    default=8, help='Gradient accumulation steps')
 parser.add_argument('--epochs', type=int, default=3, help='Number of epochs')
-parser.add_argument('--learning-rate', type=float, default=3e-4, help='Learning rate')
+parser.add_argument('--learning-rate', type=float,
+                    default=3e-4, help='Learning rate')
 parser.add_argument('--lora-r', type=int, default=8, help='LORA r')
 parser.add_argument('--lora-alpha', type=int, default=32, help='LORA alpha')
-parser.add_argument('--lora-dropout', type=float, default=0.01, help='LORA dropout')
-parser.add_argument('--max-new-tokens', type=int, default=80, help='Max new tokens')
-parser.add_argument('--temperature', type=float, default=0.1, help='Temperature')
+parser.add_argument('--lora-dropout', type=float,
+                    default=0.01, help='LORA dropout')
+parser.add_argument('--max-new-tokens', type=int,
+                    default=80, help='Max new tokens')
+parser.add_argument('--temperature', type=float,
+                    default=0.1, help='Temperature')
 parser.add_argument('--top-k', type=int, default=40, help='Top k')
 parser.add_argument('--top-p', type=float, default=0.3, help='Top p')
-parser.add_argument('--repetition-penalty', type=float, default=1.5, help='Repetition penalty')
+parser.add_argument('--repetition-penalty', type=float,
+                    default=1.5, help='Repetition penalty')
 parser.add_argument('--do-sample', action='store_true', help='Enable sampling')
 parser.add_argument('--num-beams', type=int, default=1, help='Number of beams')
-parser.add_argument('--share', action='store_true', default=False, help='Whether to deploy the interface with Gradio')
+parser.add_argument('--share', action='store_true', default=False,
+                    help='Whether to deploy the interface with Gradio')
 
 args = parser.parse_args()
 
